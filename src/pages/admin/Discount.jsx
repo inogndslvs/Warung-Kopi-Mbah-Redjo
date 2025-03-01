0910bd879b9177ede3b5192692094795c066ea33
Aufal marom <91175425+aufall02@users.noreply.github.com>
2025-03-01 19:25:02 +0700

    add page discount

---
+++ src/pages/admin/Discount.jsx
@@ -0,0 +1,10 @@
+const Discount = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold mb-4">Discount Page</h1>
+      <p>This is the Discount page.</p>
+    </div>
+  );
+};
+
+export default Discount;
